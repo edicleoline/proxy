138412b8200977efb9cce703a6079e204bc6ffd8
berners <edicleo@proton.me>
2023-03-07 17:00:32 -0300

    Removed SQLAlchemy

--- web/src/views/modems/index.js
+++ web/src/views/modems/index.js
@@ -1,4 +1,4 @@
-import { Grid, Link } from '@mui/material';
+import { Grid, Link, Box, Card, Typography } from '@mui/material';
 import MuiTypography from '@mui/material/Typography';
 
 // project imports
@@ -22,8 +22,12 @@ import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
+import { IconDotsVertical, IconAccessPoint, IconAccessPointOff } from '@tabler/icons';
+import { IconAntennaBars1, IconAntennaBars2, IconAntennaBars3, IconAntennaBars4, IconAntennaBars5 } from '@tabler/icons';
+// import { IconSignal4gPlus, IconSignal3g, IconSignal4g, IconSignal5g } from '@tabler/icons';
+import { green, grey } from '@mui/material/colors';
 
 const Modems = () => {
     // const [isLoading, setLoading] = useState(true);
@@ -77,6 +81,38 @@ const Modems = () => {
         setAnchorEl(null);
     };
 
+    const ColorBox = ({ bgcolor, title, dark }) => (
+        <>
+            <Card sx={{ mb: 0 }}>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        py: 1,
+                        bgcolor,
+                        color: dark ? 'grey.800' : '#ffffff'
+                    }}
+                >
+                    {title}
+                </Box>
+            </Card>
+        </>
+    );
+
+    const SignalBar = (data) => {
+        const icons = {
+            ['0']: <Typography variant="subtitle2">-</Typography>,
+            ['1']: <IconAntennaBars1 title={data.signal} />,
+            ['2']: <IconAntennaBars2 title={data.signal} />,
+            ['3']: <IconAntennaBars3 title={data.signal} />,
+            ['4']: <IconAntennaBars4 title={data.signal} />,
+            ['5']: <IconAntennaBars5 title={data.signal} />
+        };
+
+        return icons[data.signal];
+    };
+
     return (
         <MainCard title="Modems" secondary={<SecondaryAction link="https://next.material-ui.com/system/typography/" />}>
             <Grid container spacing={0}>
@@ -89,15 +125,14 @@ const Modems = () => {
                                         <TableHead>
                                             <TableRow>
                                                 <TableCell>Modem</TableCell>
-                                                <TableCell align="right">Status</TableCell>
+                                                <TableCell align="left">Status</TableCell>
                                                 <TableCell align="right">IP externo</TableCell>
-                                                <TableCell align="right">Rede</TableCell>
                                                 <TableCell align="right">Provedor</TableCell>
-                                                <TableCell align="right">Sinal</TableCell>
+                                                <TableCell align="right">Rede</TableCell>
+                                                <TableCell align="center">Sinal</TableCell>
                                                 <TableCell align="right">Porta proxy</TableCell>
                                                 <TableCell align="right">Status proxy</TableCell>
                                                 <TableCell align="right">Porta USB</TableCell>
-                                                <TableCell align="right">Status porta USB</TableCell>
                                                 <TableCell align="right">Tipo</TableCell>
                                                 <TableCell align="right">Modelo</TableCell>
                                             </TableRow>
@@ -110,23 +145,57 @@ const Modems = () => {
                                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                                 >
                                                     <TableCell component="th" scope="row">
-                                                        {row.modem.id}
+                                                        <IconButton
+                                                            id="basic-button"
+                                                            aria-label="expand row"
+                                                            size="small"
+                                                            aria-controls={open ? 'basic-menu' : undefined}
+                                                            aria-haspopup="true"
+                                                            aria-expanded={open ? 'true' : undefined}
+                                                            onClick={handleClick}
+                                                        >
+                                                            <IconDotsVertical />
+                                                        </IconButton>
+                                                        <Menu
+                                                            id="basic-menu"
+                                                            anchorEl={anchorEl}
+                                                            open={open}
+                                                            onClose={handleClose}
+                                                            MenuListProps={{
+                                                                'aria-labelledby': 'basic-button'
+                                                            }}
+                                                        >
+                                                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                                            <MenuItem onClick={handleClose}>My account</MenuItem>
+                                                            <Divider />
+                                                            <MenuItem onClick={handleClose}>Configurações</MenuItem>
+                                                        </Menu>
+                                                        &nbsp;&nbsp;{row.modem.id}
                                                     </TableCell>
-                                                    <TableCell align="right">{row.is_connected ? 'ON' : 'OFF'}</TableCell>
+                                                    <TableCell align="left">
+                                                        {row.is_connected ? (
+                                                            <ColorBox bgcolor={'success.light'} title="Conectado" dark />
+                                                        ) : (
+                                                            <ColorBox bgcolor={'orange.light'} title="Desconectado" dark />
+                                                        )}
+                                                    </TableCell>
                                                     <TableCell align="right">{row.external_ip ? row.external_ip : '-'}</TableCell>
-                                                    <TableCell align="right">
-                                                        {row.device_network_type ? row.device_network_type : '-'}
-                                                    </TableCell>
                                                     <TableCell align="right">
                                                         {row.device_network_provider ? row.device_network_provider : '-'}
                                                     </TableCell>
                                                     <TableCell align="right">
-                                                        {row.device_network_signalbar ? row.device_network_signalbar : '-'}
+                                                        {row.device_network_type ? row.device_network_type : '-'}
+                                                    </TableCell>
+                                                    <TableCell align="center">
+                                                        {row.device_network_signalbar ? (
+                                                            <SignalBar signal={row.device_network_signalbar} />
+                                                        ) : (
+                                                            <SignalBar signal={0} />
+                                                        )}
                                                     </TableCell>
                                                     <TableCell align="right">{row.proxy.port}</TableCell>
                                                     <TableCell align="right"></TableCell>
                                                     <TableCell align="right">{row.usb.port}</TableCell>
-                                                    <TableCell align="right">{row.usb.status}</TableCell>
                                                     <TableCell align="right">{row.modem.device.type}</TableCell>
                                                     <TableCell align="right">{row.modem.device.model}</TableCell>
                                                 </TableRow>
