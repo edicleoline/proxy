af194286810a18233f074f0fce8777e56389ff90
berners <edicleo@proton.me>
2023-04-12 15:54:12 -0300

    API Settings

--- framework/settings.py
+++ framework/settings.py
@@ -2,4 +2,4 @@ class Settings():
     def __init__(self):
         self.current_ip_before_rotate_timeout = 10
         self.current_ip_after_rotate_timeout = 30
-        self.modem_status_external_ip_interval = 30
+        self.modem_status_external_ip_interval = 1 #not used
