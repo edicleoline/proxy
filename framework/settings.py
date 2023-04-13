0fa9a97f53ea19209a99c175b61fe78ec6194626
berners <edicleo@proton.me>
2023-04-13 16:29:37 -0300

    Modem Status cloacker

--- framework/settings.py
+++ framework/settings.py
@@ -4,5 +4,5 @@ class Settings():
         self.current_ip_after_rotate_timeout = 30
         self.modem_status_external_ip_interval = 30
         self.modem_status_external_ip_timeout = 5
-        # self.external_ip_url = 'https://ipecho.net/plain'
-        self.external_ip_url = 'https://api.ipify.org/'
+        self.external_ip_url = 'https://ipecho.net/plain'
+        # self.external_ip_url = 'https://api.ipify.org/'
