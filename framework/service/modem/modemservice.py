18bfdc3e30e43cbd7cb6f87468e0e87c00156306
berners <edicleo@proton.me>
2023-04-13 15:46:36 -0300

    Modem Status cloacker

--- framework/service/modem/modemservice.py
+++ framework/service/modem/modemservice.py
@@ -297,6 +297,7 @@ class ModemsObserver():
                     external_ip = modem_state.infra_modem.external_ip_through_device(timeout = self.settings.modem_status_external_ip_timeout)
                     if external_ip:
                         self.cloacker_service.add_or_update(Cloacker(id = external_ip_cloacker_id, interval = self.settings.modem_status_external_ip_interval))
+                    elif external_ip_cloacker: external_ip_cloacker.invert()
                 except TimeoutException: pass
 
             if modem_state.is_connected != True: continue
