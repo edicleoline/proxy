e451e6736e3a506278c5e7b111c36659314be9f1
berners <edicleo@proton.me>
2023-05-04 09:47:32 -0300

    Middleware param value

--- framework/models/modemmiddlewaremodel.py
+++ framework/models/modemmiddlewaremodel.py
@@ -1,6 +1,13 @@
 from framework.models.middleware import MiddlewareModel
+from typing import List
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+from framework.models.middlewareparam import MiddlewareParamModel
 
 class ModemMiddlewareModel(MiddlewareModel):
+    params: List[MiddlewareParamModel]
+
     @property
     def params(self):
+        print('called from ext')
         return super(ModemMiddlewareModel, self).find_by_middleware_id(self.id)
