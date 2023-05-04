ac5dc9477973eae6c5b9ccfdbbad8bdf90a8d50a
berners <edicleo@proton.me>
2023-05-04 10:39:38 -0300

    Middleware param value

--- framework/models/modemmiddlewareparam.py
+++ framework/models/modemmiddlewareparam.py
@@ -5,11 +5,11 @@ from dataclasses_json import dataclass_json
 @dataclass_json
 @dataclass
 class ModemMiddlewareParamModel(MiddlewareParamModel):
-    test: str = 'hey you!'
+    test: str
 
     def __init__(self, id: int = None, middleware_id: int = None, name: str = None, name_translate: str = None, type: str = None, required: bool = True, created_at=None):
-        super().__init__(id, middleware_id, name, name_translate, type, required, created_at)
         self.modem_id: int = None
+        super().__init__(id, middleware_id, name, name_translate, type, required, created_at)        
 
     @property
     def test(self):
